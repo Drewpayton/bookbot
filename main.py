94eb74b079f14126244aee04df367f1104a673b1
Drew Payton <drewpayton18@Drews-MacBook-Pro-2.local>
2024-04-14 13:31:45 -0500

    Added a function to sort the data and arrange it in a cleaner way

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
     book_path = "./books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(count_words(text))
-    print(letter_count(text))
+    print("-- Begin report of books/frankenstein.txt --")
+    print(f"{count_words(text)} words were found in the document\n")
+    letter_count(text)
+    print("-- End Report --")
+    
+
 
 
 def get_book_text(path):
@@ -23,6 +27,14 @@ def letter_count(text):
                 count[letter] = 1
             else:
                 count[letter] += 1
-    return count
+
+    return sort_on(count)
+
+def sort_on(text):
+    text = sorted(text.items(), reverse=True, key=lambda x:x[1])
+
+    for key, value in text:
+        if str(key).isalpha():
+            print(f"The '{key}' character was found {value} times")
 
 main()
